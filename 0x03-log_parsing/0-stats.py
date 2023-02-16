9720413bb8cc9fb554e253e40276b0cf201f11f8
dejetem <enyeoluwarotimisimon@gmail.com>
2023-02-16 20:40:51 +0100

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,90 +2,49 @@
 """
 a script that reads stdin line by line and computes metrics
 """
-import re
+import sys
 
 
-def extract_input(input_line):
-    """
-    Extracts sections of HTTP request log
-    """
-    fp = (
-        r'\s*(?P<ip>\S+)\s*',
-        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
-        r'\s*"(?P<request>[^"]*)"\s*',
-        r'\s*(?P<status_code>\S+)',
-        r'\s*(?P<file_size>\d+)'
-    )
-    info = {
-        'status_code': 0,
-        'file_size': 0,
-    }
-    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
-    resp_match = re.fullmatch(log_fmt, input_line)
-    if resp_match is not None:
-        status_code = resp_match.group('status_code')
-        file_size = int(resp_match.group('file_size'))
-        info['status_code'] = status_code
-        info['file_size'] = file_size
-    return info
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+
+file_size = 0
 
 
-def print_statistics(total_file_size, status_code):
+def print_metrics():
     """
-    the accumulated statistics of the HTTP request log
+    logs
     """
-    print('File size: {:d}'.format(total_file_size), flush=True)
-    for status_code in sorted(status_code.keys()):
-        num = status_code.get(status_code, 0)
-        if num > 0:
-            print('{:s}: {:d}'.format(status_code, num), flush=True)
+    print("File size: {}".format(file_size))
+    for status in sorted(status_codes.keys()):
+        if status_codes[status]:
+            print("{}: {}".format(status, status_codes[status]))
 
 
-def update_metrics(line, total_file_size, status_code):
-    """
-    Updates the metrics from a given HTTP request log.
-    Args:
-        line (str): The line of input from which to retrieve the metrics.
-    Returns:
-        int: The new total file size.
-    """
-    line_info = extract_input(line)
-    status_code = line_info.get('status_code', '0')
-    if status_code in status_code.keys():
-        status_code[status_code] += 1
-    return total_file_size + line_info['file_size']
-
-
-def run():
-    """
-    log parser
-    """
-    num = 0
-    total_file_size = 0
-    status_code = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0,
-    }
+if __name__ == "__main__":
+    count = 0
     try:
-        while True:
-            line = input()
-            total_file_size = update_metrics(
-                line,
-                total_file_size,
-                status_code,
-            )
-            num += 1
-            if num % 10 == 0:
-                print_statistics(total_file_size, status_code)
-    except (KeyboardInterrupt, EOFError):
-        print_statistics(total_file_size, status_code)
-
-
-if __name__ == '__main__':
-    run()
+        for line in sys.stdin:
+            try:
+                elems = line.split()
+                file_size += int(elems[-1])
+                if elems[-2] in status_codes:
+                    status_codes[elems[-2]] += 1
+            except Exception:
+                pass
+            if count == 9:
+                print_metrics()
+                count = -1
+            count += 1
+    except KeyboardInterrupt:
+        print_metrics()
+        raise
+    print_metrics()
