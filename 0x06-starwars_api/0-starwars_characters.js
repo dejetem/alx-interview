a3d0f1049de730113b0cce48a9789a7882562859
dejetem <enyeoluwarotimisimon@gmail.com>
2023-03-07 18:57:04 +0100

    done

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+/**
+ * a script that prints all characters of a Star Wars movie
+ * use star wars api
+ */
+const request = require('request');
+const API_URL = 'https://swapi-api.alx-tools.com/api/';
+// const process = ('node:process')
+
+
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
+}
