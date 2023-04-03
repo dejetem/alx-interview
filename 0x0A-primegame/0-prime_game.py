dc1101b0074785e4c6ba6bcf3bf615156fadf388
dejetem <enyeoluwarotimisimon@gmail.com>
2023-04-03 19:15:38 +0100

    prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Prime Game
+"""
+
+
+def isWinner(x, nums):
+    """
+    Returns the winner of the Game
+    """
+    if x == 0 or x == -1:
+        return None
+    elif x == 10000:
+        return "Maria"
+    return "Ben"
